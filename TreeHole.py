f45aced91e1b0a9405d2ee81bdde85d0039bfc4d
王子旗 <wangzq96@126.com>
2018-03-20 14:03:19 +0800

    测试通过
    面试前测试通过

--- TreeHole.py
+++ TreeHole.py
@@ -199,34 +199,17 @@ class delete:
         raise web.seeother("/user/%s" %cookei)
 
 class check:
-    def GET(self, checktime):
-        if time.time()-float(checktime) < 7776000:
-            web.header("Content-Type","text/html; charset=utf-8")
-            return """<html><body><center><h4>还没到时候呢</h4></body></html>"""
-        else:
-            cur = conn.cursor()
-            aa = cur.execute("select * from data where time='%s'" %checktime)
-            info = cur.fetchmany(aa)
-            cur.close()
-            nowtime = time.strftime('%Y-%m-%d %X', time.localtime(float(checktime)))
-            web.header("Content-Type","text/html; charset=utf-8")
-            return """
-            <html>
-            <body>
-            <div style="width:800px;margin:0 auto;background:#FCC">
-            <dt>
-            time: %s<br>
-            user: %s
-            </dt>
-            </div>
-            <div style="width:800px;margin:0 auto;background:#CCC">
-                <dd>
-                %s
-                </dd><br>
-            </div><br>
-            </body>
-            </html>
-            """ %(nowtime, info[0][0], info[0][2])
+	def GET(self, filename):
+		BUF_SIZE = 262144
+		filedir = 'C:\Users\wangz\Desktop'
+		fout = open(filedir +'\\'+ filename,'rb')
+		while True:
+			c = fout.read(BUF_SIZE)
+			if c:
+				yield c
+			else:
+				break
+		fout.close()
 
 class newuser:
     def GET(self):
@@ -395,78 +378,173 @@ class newuser:
             raise web.seeother("/")
 
 class user:
-    def GET(self, username):
-        cookei = web.cookies().get('username')
-        if cookei == username:
-            cur = conn.cursor()
-            aa = cur.execute("select * from data")
-            info = cur.fetchmany(aa)
-            cur.close()
-            addtext=""
-            for temp in info:
-                temptime = temp[1]
-                nowtime = time.strftime('%Y-%m-%d %X', time.localtime(float(temptime)))
-                nowtext=''
-                color = '#CCF'
-                option = 'delete'
-                if temp[0] != username:
-                    nowtext = '??????'
-                    color = '#FCC'
-                    option = 'check'
-                else:
-                    nowtext = temp[2]
-                addtext = addtext + """
-                    <div style="width:800px;margin:0 auto;background:%s">
-                    <dt>
-                    time: %s<br>
-                    user: %s<br>
-                    <a href="/%s/%s">%s</a>
-                    </dt>
-                    </div>
-                    <div style="width:800px;margin:0 auto;background:#CCC">
-                        <dd>
-                        %s
-                        </dd><br>
-                    </div><br>
-                    """ %(color, nowtime, temp[0], option, temptime, option, nowtext)
-            web.header("Content-Type","text/html; charset=utf-8")
-            return """
-            <html>
-            <body>
-            当前用户:%s<br>
-            <a href="/logout">退出登录</a>
-            <br>
-            <form method="POST">
-            <center>
-            <h3>
-            写下你要说的话吧～
-            </h3>
-            <textarea rows="15" cols="70" name="Test"></textarea>
-            <br>
-            <input type="submit" value="提交咯">
-            </form>
-            </center>
-            <dl>
-            %s
-            </dl>
-            </body>
-            </html>
-            """ %(username.encode('utf8'),addtext.encode('utf8'))
-        else:
-            raise web.seeother("/")
+	def GET(self, username):
+		cookei = web.cookies().get('username')
+		if cookei == username:
+			cur = conn.cursor()
+			aa = cur.execute("select * from data")
+			info = cur.fetchmany(aa)
+			cur.close()
+			addtext=""
+			for temp in info:
+				temptime = temp[1]
+				tempsavetime = temp[3]
+				tempfilename = temp[4]
+				option = ""
+				if tempfilename !="":
+					option = """<a href="/check/%s">attachment</a><br>"""%(tempfilename)
+				nowtime = time.strftime('%Y-%m-%d %X', time.localtime(float(temptime)))
+				nowtext=''
+				if temp[0] != username:
+					if time.time()-float(temptime) < float(tempsavetime):
+						nowtext = '??????'
+						color='#FCC'
+						remaintime=float(tempsavetime)-time.time()+float(temptime)
+						option = "" 
+					else:
+						nowtext = temp[2]
+						color = '#CFC'
+						remaintime='0';
+					addtext = addtext + """
+						<div style="width:800px;margin:0 auto;background:%s">
+						<dt>
+						time: %s<br>
+						user: %s<br>
+						remain: %sS<br>
+						%s
+						</dt>
+						</div>
+						<div style="width:800px;margin:0 auto;background:#CCC">
+							<dd>
+							%s
+							</dd><br>
+						</div><br>
+						""" %(color, nowtime, temp[0], remaintime, option, nowtext)
+				else:
+					nowtext = temp[2]
+					addtext = addtext + """
+						<div style="width:800px;margin:0 auto;background:#CCF">
+						<dt>
+						time: %s<br>
+						user: %s<br>
+						<a href="/delete/%s">delete</a><br>
+						%s
+						</dt>
+						</div>
+						<div style="width:800px;margin:0 auto;background:#CCC">
+							<dd>
+							%s
+							</dd><br>
+						</div><br>
+						""" %(nowtime, temp[0], temptime, option, nowtext)
+			web.header("Content-Type","text/html; charset=utf-8")
+			return """
+			<html>
+			<body bgcolor="#C0C0C0">
+			<div style="width:100px;
+						height:100px;
+						background:#525252;
+						box-shadow:#666 0px 0px 6px;
+						border-radius:4px">
 
-    def POST(self, username):
-        x = web.input()
-        temptext = x.Test
-        nowtime = time.time()
-        cur = conn.cursor()
-        cur.execute("insert into data values('%s','%s','%s')" %(username,nowtime,temptext))
-        conn.commit()
-        cur.close()
-        web.setcookie('username',username,3600)
-        raise web.seeother("/user/%s" %username)
+				<center>
+
+				<div style="width:100px;
+							height:30px;
+							background:#606060;
+							line-height:30px;
+							border-radius:4px;
+							text-shadow:1px 1px 1px #949494">
+
+					<font color="#DFDFDF">当前用户</font>
+
+				</div>
+
+				<font color="#DFDFDF">%s</font>
+				<br>
+				<br>
+				<a href="/logout" style="width:90px;
+									  height:30px;
+									  padding:5px 10px 5px 10px;
+									  font-size:14px;
+									  background:#E27575;
+									  text-decoration:none;
+									  border:none;
+									  color:#585858;
+									  border-radius:4px;
+									  box-shadow:1px 1px 1px #3D3D3D;
+									  -webkit-appearance:none;
+									  text-shadow:1px 1px 1px #949494;">退出登陆</a>
+
+			</div>
+			<br>
+			<form method="POST" enctype="multipart/form-data" action="">
+			<center>
+			<h2>
+			留言
+			</h2>
+			<textarea style="border:none;
+			                 background:#DFDFDF;
+							 border-radius:4px;
+							 " rows="15" cols="70" name="Test"></textarea>
+			<br>
+			附件
+			<input type="file" name="myfile" />
+			<br>
+			<br>
+			<input type="submit" style="width:90px;
+										height:30px;
+										font-size:14px;
+										background:#9DC45F;
+										border:none;
+										color:#585858;
+										border-radius:4px;
+										box-shadow:1px 1px 1px #3D3D3D;
+										text-shadow:1px 1px 1px #949494;
+										-webkit-appearance:none;"
+										value="提交">
+			<input type="radio" name="savetime" value="none" checked>即时
+			<input type="radio" name="savetime" value="day">一天
+			<input type="radio" name="savetime" value="week">一周
+			</center>
+			</form>
+			<dl>
+			%s
+			</dl>
+			</body>
+			</html>
+			""" %(username.encode('utf8'),addtext.encode('utf8'))
+		else:
+			raise web.seeother("/")
+
+	def POST(self, username):
+		x = web.input(myfile={})
+		nowtime = time.time()
+		temptext = x.Test
+		cur = conn.cursor()
+		tempsavetime = "0";
+		if x.savetime == "none":
+			tempsavetime = "0"
+		else:
+			if x.savetime == "day":
+				tempsavetime = "86400"
+			else:
+				tempsavetime = "604800"
+		filedir = 'C:\Users\wangz\Desktop'
+		if 'myfile' in x:
+			filepath=x.myfile.filename.replace('\\','/')
+			filename=filepath.split('/')[-1]
+			if filename != "":
+				fout = open(filedir +'\\'+ filename,'wb')
+				fout.write(x.myfile.file.read())
+				fout.close()
+		cur.execute("insert into data values('%s','%s','%s','%s','%s')" %(username,nowtime,temptext,tempsavetime,filename))
+		conn.commit()
+		cur.close()
+		web.setcookie('username',username,3600)
+		raise web.seeother("/user/%s" %username)
 
 if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.run()
+	app = web.application(urls, globals())
+	app.run()
 
